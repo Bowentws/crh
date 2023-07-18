5d93468256facec406e8130d8fed2eed7a45d6c6
Bowentws <133575567+Bowentws@users.noreply.github.com>
2023-07-18 10:17:57 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,10 +14,7 @@ SessionSecret = 7cF5vIg8XLKtFOLU2kFNgz6zAkDS8w2Lyw1qSlIkEAod5TewkKPqU9X9b4CtH3qS
 ; Hash 加盐, 一般在首次启动时自动生成
 HashIDSalt = kfIWhhcteuRACDnXtgkilGDgOdI8c6j2Prn9lUzossrOZn1pi46SXb80XFAaKMF3
 
-[Redis]
-Server = Redis地址：端口
-Password = Redis密码
-DB = 0
+
 EOF
 
 /root/cloudreve/cloudreve -c /root/cloudreve/conf.ini
