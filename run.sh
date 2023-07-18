b2c2d49a13e5aabe4e31299b8cc6d44079de252f
Bowentws <133575567+Bowentws@users.noreply.github.com>
2023-07-18 10:16:55 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,14 +13,7 @@ Debug = false
 SessionSecret = 7cF5vIg8XLKtFOLU2kFNgz6zAkDS8w2Lyw1qSlIkEAod5TewkKPqU9X9b4CtH3qS
 ; Hash 加盐, 一般在首次启动时自动生成
 HashIDSalt = kfIWhhcteuRACDnXtgkilGDgOdI8c6j2Prn9lUzossrOZn1pi46SXb80XFAaKMF3
-[Database]
-Type = mysql
-Port = 3306
-User = 数据库用户名
-Password = 数据库密码
-Host = 数据库地址
-Name = 	数据库名称
-TablePrefix = wd
+
 [Redis]
 Server = Redis地址：端口
 Password = Redis密码
